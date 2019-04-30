d3a3d37b56329a35088db88fa78f89c3aa7cf7b5
John Honniball <coredump@virginmedia.com>
2019-04-30 20:31:53 +0100

    Add a few comments.

--- RGBIsoGrid.ino
+++ RGBIsoGrid.ino
@@ -7,8 +7,8 @@
 #define NUM_LEDS    (49)
 #define BRIGHTNESS  (64)
 
-#define BUTTON_PIN  (2)
-#define DATA_PIN    (3)
+#define BUTTON_PIN  (2)   // A push-button switch connected to ground, with a pull-up resistor
+#define DATA_PIN    (3)   // Connection to the WS2812 LED chain
 
 
 CRGB Leds[NUM_LEDS];
@@ -18,6 +18,7 @@ struct polar {
   unsigned int theta;
 };
 
+// LED positions expressed as polar co-ordinates
 const struct polar Posn[NUM_LEDS] = {
   {192, 120}, {192, 100}, {192, 80},  {192, 60},                                      // 0-3
   {192, 40},  {128, 60},  {128, 90},  {128, 120}, {192, 140},                         // 4-8
@@ -30,12 +31,14 @@ const struct polar Posn[NUM_LEDS] = {
   {255, 120}, {255, 90},  {255, 60},  {255, 30},  {255, 0},   {255, 330}              // 43-48
 };
 
+// Concentric hexagons, in terms of LED numbers
 const unsigned char hex0[] = {18, 255};
 const unsigned char hex1[] = {11, 12, 17, 25, 24, 19, 255};
 const unsigned char hex2[] = {7, 6, 5, 13, 16, 26, 29, 30, 31, 23, 20, 10, 255};
 const unsigned char hex3[] = {0, 1, 2, 3, 4, 14, 15, 27, 28, 36, 35, 34, 33, 32, 22, 21, 9, 8, 255};
 const unsigned char hex4[] = {43, 44, 45, 46, 47, 48, 37, 38, 39, 40, 41, 42, 255};
 
+// Horizontal rows on the hex grid, in terms of LED numbers
 const unsigned char row0[] = {43, 44, 45, 255};
 const unsigned char row1[] = {0, 1, 2, 3, 255};
 const unsigned char row2[] = {42, 8, 7, 6, 5, 4, 46, 255};
@@ -46,6 +49,7 @@ const unsigned char row6[] = {40, 32, 31, 30, 29, 28, 48, 255};
 const unsigned char row7[] = {33, 34, 35, 36, 255};
 const unsigned char row8[] = {39, 38, 37, 255};
 
+// Decimal digits 0-9, in terms of LED numbers
 const unsigned char digit0[] = {0, 1, 2, 3, 4, 14, 15, 27, 28, 36, 35, 34, 33, 32, 22, 21, 9, 8, 255};
 const unsigned char digit1[] = {0, 7, 11, 18, 25, 29, 36, 255};
 const unsigned char digit2[] = {3, 2, 1, 0, 8, 9, 20, 19, 18, 17, 16, 27, 28, 36, 35, 34, 33, 255};
@@ -57,6 +61,7 @@ const unsigned char digit7[] = {0, 7, 11, 17, 18, 19, 25, 29, 36, 35, 34, 33, 25
 const unsigned char digit8[] = {0, 1, 2, 3, 4, 8, 9, 14, 27, 28, 36, 35, 34, 33, 32, 22, 20, 19, 18, 17, 16, 255};
 const unsigned char digit9[] = {0, 1, 2, 3, 4, 14, 15, 27, 28, 36, 35, 34, 33, 32, 22, 20, 19, 18, 17, 16, 255};
 
+// Two pre-set frames of animation
 const long Frame01[NUM_LEDS] PROGMEM =
 {
 0xff0000, 0x000000, 0x000000, 0x00ff00, 0x000000, 0x00ff00, 0x000000, 0xff0000, // 0-7
@@ -79,6 +84,7 @@ const long Frame02[NUM_LEDS] PROGMEM =
 0x00ffff                                                                        // 48
 };
 
+// Some random LED colours
 const CRGB RandomColours[12] = {
   CRGB::Red, CRGB::Yellow, CRGB::Lime, CRGB::White,   CRGB::Blue, CRGB::Yellow, 
   CRGB::Red, CRGB::White,  CRGB::Lime, CRGB::Magenta, CRGB::Lime, CRGB::White
@@ -96,15 +102,15 @@ void setup(void)
 
 void loop(void)
 {
-  int ana;
+  int ana;        // Analog pot reading, 0-1023
   int led, level;
   int frame;
   int i;
   int hue;
   int dot = 0;
   int dir = 1;
-  int digit = 0;
-  int mode = 0;
+  int digit = 0;  // Digit 0-9
+  int mode = 0;   // Demo mode, selected by the push-button switch
 
   for (frame = 0; ; frame++) {
     ana = analogRead(0);
@@ -113,10 +119,10 @@ void loop(void)
     led = map(ana, 0, 1023, 0, NUM_LEDS - 1);
 
     switch (mode) {
-    case 0:
+    case 0:   // Just show a fixed colour, with adjustable brightness
       FastLED.showColor(CRGB::Yellow, level);
       break;
-    case 1:
+    case 1:   // Show an adjustable hue, at fixed brightness
       FastLED.showColor(CHSV(level, 255, 255), BRIGHTNESS);
       break;
     case 2:
@@ -165,7 +171,7 @@ void loop(void)
 
       FastLED.show();
       break;
-    case 7:
+    case 7:   // Hue-cycling by concentric hexagons
       FastLED.clear();
       
       sethex(hex0, CRGB::White);
@@ -175,7 +181,7 @@ void loop(void)
       sethex(hex4, CHSV((level + 192) & 255, 255, 255));
       FastLED.show();
       break;
-    case 8:
+    case 8:   // Hue-cycling by row
       FastLED.clear();
       
       sethex(row0, CHSV(level, 255, 255));
@@ -189,7 +195,7 @@ void loop(void)
       sethex(row8, CHSV((level + 228) & 255, 255, 255));
       FastLED.show();
       break;
-    case 9:
+    case 9:   // Rotating hue-cycle
       FastLED.clear();
 
       for (i = 0; i < NUM_LEDS; i++) {
@@ -206,7 +212,7 @@ void loop(void)
 
       FastLED.show();
       break;
-    case 10:
+    case 10:  // Display digits 0-9
       FastLED.clear();
       if ((frame % 75) == 0)
         if (digit < 9)
